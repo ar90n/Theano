93f2ecce5f8cfd05b69e43fea580f3f3e604c198
Masahiro Wada <argon.argon.argon@gmail.com>
2017-02-05 16:10:36 +0900

    wasure

--- theano/sandbox/rng_mrg.py
+++ theano/sandbox/rng_mrg.py
@@ -937,11 +937,11 @@ class GPUA_mrg_uniform(GpuKernelBase, mrg_uniform_base):
                 x21 = y2;
 
                 if (x11 <= x21) {
-                    %(write)s(sample_data[i], (((x11 - x21 + M1) %(mask)s) * %(NORM)s));
+                    %(write)s((sample_data + i), (((x11 - x21 + M1) %(mask)s) * %(NORM)s));
                 }
                 else
                 {
-                    %(write)s(sample_data[i],( ((x11 - x21) %(mask)s) * %(NORM)s));
+                    %(write)s((sample_data + i), (((x11 - x21) %(mask)s) * %(NORM)s));
                 }
             }
 
